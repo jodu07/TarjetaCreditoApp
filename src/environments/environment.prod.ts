1fbf8bfc199c7141d1750509d6bdc25e91ca4678
jodu07 <jhonnat.dussan@gmail.com>
2022-04-23 22:31:23 -0500

    setting firebase: {

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCVSbpKcdVXFIJDxU8xWMV8TzbpMNDOE70",
+    authDomain: "tarjetacredito-69966.firebaseapp.com",
+    projectId: "tarjetacredito-69966",
+    storageBucket: "tarjetacredito-69966.appspot.com",
+    messagingSenderId: "683249385460",
+    appId: "1:683249385460:web:bb2fda47621aeccf2443a1"
+  }  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCVSbpKcdVXFIJDxU8xWMV8TzbpMNDOE70",
+    authDomain: "tarjetacredito-69966.firebaseapp.com",
+    projectId: "tarjetacredito-69966",
+    storageBucket: "tarjetacredito-69966.appspot.com",
+    messagingSenderId: "683249385460",
+    appId: "1:683249385460:web:bb2fda47621aeccf2443a1"
+  }
 };
 
 /*
