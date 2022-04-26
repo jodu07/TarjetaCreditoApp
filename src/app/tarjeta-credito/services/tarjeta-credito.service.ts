d675d34e72aea012f6cd217dfcb61741e0571814
jodu07 <jhonnat.dussan@gmail.com>
2022-04-25 22:46:54 -0500

    saveTarjeta

--- src/app/tarjeta-credito/services/tarjeta-credito.service.ts
+++ src/app/tarjeta-credito/services/tarjeta-credito.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { TarjetaCredito } from '../models/TarjetaCredito';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TarjetaCreditoService {
 
+  private tarjeta$ = new Subject<any>();
+
   constructor(private firestore: AngularFirestore) { }
 
   getTarjetas(): Observable<any>{
     return this.firestore.collection('tarjetas', ref => ref.orderBy('fechaCreacion', 'asc')).snapshotChanges();
   }
+
+
+  guardarTarjeta(tarjeta: TarjetaCredito): Promise<any> {
+    return this.firestore.collection('tarjetas').add(tarjeta);
+  }
+
 
 }
